a76b9e19526561d18a4488a18d58dcbd3687cef4
it-is-nidah <leonidakoech@gmail.com>
2023-07-25 17:05:04 -0500

    Initial Commit

---
+++ WarCardGame/WarCardGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  WarCardGameApp.swift
+//  WarCardGame
+//
+//  Created by Leonidah Chepkoech on 7/25/23.
+//
+
+import SwiftUI
+
+@main
+struct WarCardGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
