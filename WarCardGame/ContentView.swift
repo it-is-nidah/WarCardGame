2e2464462b9bdc53c654f20ea9ca7df8eb97fa0d
it-is-nidah <leonidakoech@gmail.com>
2023-07-28 13:15:12 -0500

    changed background

--- WarCardGame/ContentView.swift
+++ WarCardGame/ContentView.swift
@@ -10,30 +10,47 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         ZStack {
-            Image("background-plain")
+            Image("background-cloth")
+                .resizable()
+                .ignoresSafeArea()
             VStack {
+                Spacer()
                 Image("logo")
+                Spacer()
                 HStack {
+                    Spacer()
                     Image("card2")
+                    Spacer()
                     Image("card3")
+                    Spacer()
                 }
-                
+                Spacer()
                 Image("button")
+                Spacer()
                 HStack {
+                    Spacer()
                     VStack {
                         Text("Player")
+                            .font(.headline)
+                            .padding(.bottom, 10.0)
                         Text("0")
+                            .font(.largeTitle)
                     }
-                    
+                    Spacer()
                     VStack {
                         Text("CPU")
+                            .font(.headline)
+                            .padding(.bottom, 10.0)
                         Text("0")
+                            .font(.largeTitle)
                     }
-                    
+                    Spacer()
                 }
                 .foregroundColor(.white)
-                .font(.title2)
+            
+                Spacer()
             }
+            
         }
 
     }
